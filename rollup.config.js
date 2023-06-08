3873c8944345d450c544b35c02a69f9749b69b20
JIMENEZ PEREZ Irene <irene.jimenez2@soprasteria.com>
2023-06-08 11:42:45 +0200

    update config to deploy

--- rollup.config.js
+++ rollup.config.js
@@ -63,7 +63,7 @@ export default {
       // directory to match patterns against to be precached
       globDirectory: path.join('dist'),
       // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest,svg,png,ico}'],
+      globPatterns: ['**/*.{html,js,css,webmanifest,svg,png,ico,mp3}'],
       skipWaiting: true,
       clientsClaim: true,
       runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
